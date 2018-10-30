f71059de7fad260dcc507e93733b52d81f35452c
530 <linyf@shushangyun.com>
2018-10-30 23:24:50 +0800

    Initial commit

---
+++ src/components/HeaderId.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>执行顺序</h2>
+    <h2>Ecosystem</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderId',
+  data () {
+    return {
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// 导入自定义组件
+import HeaderId from '@/components/HeaderId'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+// 声明自定义组件
+export default new Router({
+  // 组件首次执行的页面序列是路由的第一个
+  routes: [
+    {
+      path: '/',
+      name: 'HeaderId',
+      component: HeaderId
+    },
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
