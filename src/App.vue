b75f37ab1e212a6cb2c1e540e3fded8223e3bdd9
530 <linyf@shushangyun.com>
2018-10-30 23:30:12 +0800

    添加注释

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,19 @@
   </div>
 </template>
 
+<!--
+  这里一般不需要动
+1、main.js是我们的入口文件，主要作用是初始化vue实例，并引入所需要的插件
+2、App.vue是我们的主组件，所有页面都是在App.vue下进行切换的。其实你也可以理解为所有的路由也是App.vue的子组件。
+   所以我将router标示为App.vue的子组件。
+3、index.html文件入口
+4、src放置组件和入口文件
+5、node_modules为依赖的模块
+6、config中配置了路径端口值等
+7、build中配置了webpack的基本配置、开发环境配置、生产环境配置等
+
+-->
+
 <script>
 export default {
   name: 'App'
